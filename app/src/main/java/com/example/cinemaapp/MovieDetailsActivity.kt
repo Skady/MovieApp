da860db7ffaaa30990a94869194e621bf066c28a
Nadir Mayra Mamani Mamani <skady.nm3@gmail.com>
2021-03-04 14:14:02 +0000

    Implement check when click add to fav

--- app/src/main/java/com/example/cinemaapp/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/cinemaapp/MovieDetailsActivity.kt
@@ -2,20 +2,13 @@ package com.example.cinemaapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.ViewModelProvider
-import com.example.cinemaapp.Adapters.MoviesRecyclerAdapter
-import com.example.cinemaapp.Database.AppDatabase
-import com.example.cinemaapp.Models.DataManager
 import com.example.cinemaapp.Models.MovieModel
 import com.example.cinemaapp.ViewModels.MovieDetailViewModel
 import com.squareup.picasso.Picasso
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_movie_details.*
-import kotlinx.android.synthetic.main.content_main_list_all_movies.*
 
 class MovieDetailsActivity : AppCompatActivity() {
 
@@ -27,20 +20,25 @@ class MovieDetailsActivity : AppCompatActivity() {
 
         viewModel = ViewModelProvider(this).get(MovieDetailViewModel::class.java)
 
-        val favButton = findViewById<Button>(R.id.favButton)
-
+        var isInDB: Boolean = false
         title = intent.getStringExtra(TITLE)
+
+        viewModel.getOneMovieFromFavList(title as String)
+        viewModel.selectedMovieIsInDB.observe(this, Observer {
+            isInDB = it
+        })
 
         favButton.setOnClickListener {
             val selectedMovie = getSelectedMovie()
-            //viewModel.removeMovieFromFavList(selectedMovie)
-            if(DataManager.findMovie(title as String) == null) {
-                favButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_favorite_red_24)
-                viewModel.addMovieToFavList(selectedMovie)
-            }
-            else {
+            if(isInDB) {
                 favButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_favorite_grey_24)
                 viewModel.removeMovieFromFavList(selectedMovie)
+                flagExist.setText("true from click")
+            }
+            else {
+                favButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_favorite_red_24)
+                viewModel.addMovieToFavList(selectedMovie)
+                flagExist.setText("false from click")
             }
         }
         showMovie()
@@ -66,30 +64,23 @@ class MovieDetailsActivity : AppCompatActivity() {
     }
 
     private fun showMovie() {
-
-        val titleElement = intent.getStringExtra(TITLE)
-
-        titleDetailTextView.setText(titleElement)
+        titleDetailTextView.setText(intent.getStringExtra(TITLE))
         ratingDetailTextView.setText(intent.getStringExtra(VOTE_AVERAGE))
         summaryDetailTextView.setText(intent.getStringExtra(OVERVIEW))
 
         val urlImage = intent.getStringExtra(COMPLETE_POSTER_PATH)
         Picasso.get().load(urlImage).into(detailImageView)
 
-        viewModel.getOneMovieFromFavList(titleElement as String)
-
+        viewModel.getOneMovieFromFavList(title as String)
         viewModel.selectedMovieIsInDB.observe(this, Observer {
-            flagExist.setText("entre")
             if(it) {
                 favButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_favorite_red_24)
                 flagExist.setText("true")
             }
             else {
-                favButton.background =
-                    AppCompatResources.getDrawable(this, R.drawable.ic_favorite_grey_24)
+                favButton.background = AppCompatResources.getDrawable(this, R.drawable.ic_favorite_grey_24)
                 flagExist.setText("false")
             }
         })
     }
-
 }
